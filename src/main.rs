1cf26552f9930f055ce6d19669d72196370c7d08
Krish-Sage <6kreesh@example.com>
2026-06-24 17:17:03 +0530

    added function to clear board

--- src/main.rs
+++ src/main.rs
@@ -10,7 +10,22 @@ struct OX {
     h: char,
     i: char,
 }
+
 impl OX {
+    fn board_state(&self) {
+        let a = self.a;
+        let b = self.b;
+        let c = self.c;
+        let d = self.d;
+        let e = self.e;
+        let f = self.f;
+        let g = self.g;
+        let h = self.h;
+        let i = self.i;
+        println!(
+            "+----+----+----+\n| {a}  | {b}  | {c}  | \n+----+----+----+\n| {d}  | {e}  | {f}  | \n+----+----+----+\n| {g}  | {h}  | {i}  | \n+----+----+----+\n"
+        );
+    }
     fn game_over(&self, p: char) -> bool {
         let a = self.a;
         let b = self.b;
@@ -22,9 +37,6 @@ impl OX {
         let h = self.h;
         let i = self.i;
         let mut win: bool = false;
-        println!(
-            "+----+----+----+\n| {a}  | {b}  | {c}  | \n+----+----+----+\n| {d}  | {e}  | {f}  | \n+----+----+----+\n| {g}  | {h}  | {i}  | \n+----+----+----+\n"
-        );
         if (a == p && b == p && c == p)
             || (d == p && e == p && f == p)
             || (g == p && h == p && i == p)
@@ -38,8 +50,12 @@ impl OX {
         }
         win
     }
-    fn manuplicate_ox(&mut self, n: u8, p: char) -> bool {
-        let mut win = true;
+    fn clearing() {
+        print!("\x1B[2J\x1B[1;1H");
+    }
+
+    fn manuplicate_ox(&mut self, n: u8, p: char) -> u8 {
+        let mut err = 1;
         if n == 1 && self.a == '_' {
             self.a = p;
         } else if n == 2 && self.b == '_' {
@@ -58,11 +74,14 @@ impl OX {
             self.h = p;
         } else if n == 9 && self.i == '_' {
             self.i = p;
+        } else if n > 9 {
+            err = 3;
         } else {
-            win = false;
+            err = 2;
         }
-        win
+        err
     }
+    // Player input just takes input
     fn playerinput() -> u8 {
         let mut pinput = String::new();
         io::stdin()
@@ -92,30 +111,46 @@ fn input() {
             let p1 = 'X';
             println!("Turn: Player 1 (X)\n");
             eprint!("Enter your move(1-9): ");
-            if players.manuplicate_ox(OX::playerinput(), p1) {
+            if players.manuplicate_ox(OX::playerinput(), p1) == 1 {
                 if players.game_over(p1) {
-                    println!("Yay, Player 1 (X) won !");
+                    players.board_state();
+                    println!("Yay, Player 1 ( X ) won !");
                     break 'outer;
                 }
-            } else {
+                OX::clearing();
+                players.board_state();
+            } else if players.manuplicate_ox(OX::playerinput(), p1) == 2 {
                 println!("\nOX: baka! Can't you see it is already pre-occupied\n");
-                players.game_over(p1);
+                players.board_state();
+                continue 'p1;
+            } else {
+                println!("Baka ! Enter number in between 1 to 9");
+                players.board_state();
                 continue 'p1;
             }
+            //           OX::clearing();
+
             break 'p1;
         }
         'p2: loop {
+            let p2 = 'O';
             println!("Turn: Player 2 (O)\n");
             eprint!("Enter your move(1-9): ");
-            let p2 = 'O';
-            if players.manuplicate_ox(OX::playerinput(), p2) {
+            if players.manuplicate_ox(OX::playerinput(), p2) == 1 {
                 if players.game_over(p2) {
-                    println!("Yay, Player 2 (O) won !");
+                    players.board_state();
+                    println!("Yay, Player 2 ( O ) won !");
                     break 'outer;
                 }
-            } else {
+                OX::clearing();
+                players.board_state();
+            } else if players.manuplicate_ox(OX::playerinput(), p2) == 2 {
                 println!("\n1OX: baka! Can't you see it is already pre-occupied \n");
-                players.game_over(p2);
+                players.board_state();
+                continue 'p2;
+            } else {
+                println!("Baka ! Enter number in between 1 to 9");
+                players.board_state();
                 continue 'p2;
             }
             break 'p2;
