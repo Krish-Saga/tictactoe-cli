126cb479b2faa3de37c8f76a5358928ef5621e36
Krish-Sage <6kreesh@example.com>
2026-06-26 06:08:05 +0530

    added a dumb random move making bot

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,11 @@
+use rand::prelude::*;
 use std::io;
-struct Player {
-    letter: char,
-}
 struct OX([char; 9]);
+struct Player;
+struct DumbComputer;
 
 impl OX {
-    fn board_state(&self) {
+    fn board_state(state: Vec<char>) {
         print!("\x1B[2J\x1B[1;1H");
         println!(
             "                               ==============================\n                                       O X  G A M E\n                               =============================="
@@ -13,15 +13,15 @@ impl OX {
         println!("Player 1 (X)\n------------\n    vs\n------------\nPlayer 2 (O)\n");
         println!(
             "+----+----+----+\n| {}  | {}  | {}  | \n+----+----+----+\n| {}  | {}  | {}  | \n+----+----+----+\n| {}  | {}  | {}  | \n+----+----+----+\n",
-            self.0[0],
-            self.0[1],
-            self.0[2],
-            self.0[3],
-            self.0[4],
-            self.0[5],
-            self.0[6],
-            self.0[7],
-            self.0[8]
+            state[0],
+            state[1],
+            state[2],
+            state[3],
+            state[4],
+            state[5],
+            state[6],
+            state[7],
+            state[8]
         );
     }
     fn win_check(&self, letter: char) -> bool {
@@ -50,11 +50,10 @@ impl OX {
         win
     }
 
-    fn manuplicate_ox(&mut self, square: usize, letter: char) -> bool {
+    fn manuplicate_ox(&mut self, square: usize, letter: char) {
         if self.0[square - 1] == '_' {
             self.0[square - 1] = letter;
         }
-        true
     }
     fn available_moves(&self) -> Vec<usize> {
         let mut avail_mov: Vec<usize> = Vec::new();
@@ -65,6 +64,14 @@ impl OX {
         }
         avail_mov
     }
+
+    fn state(&self) -> Vec<char> {
+        let mut state: Vec<char> = Vec::new();
+        for i in 0..9 {
+            state.push(self.0[i]);
+        }
+        state
+    }
     fn empty_squares(&self) -> usize {
         let mut square = 0;
         for i in 1..=9 {
@@ -74,12 +81,12 @@ impl OX {
         }
         square
     }
-    fn get_move(&self, letter: char) -> usize {
+}
+impl Player {
+    fn get_move(available_moves: Vec<usize>, state: Vec<char>, letter: char) -> usize {
         let mut valid_square = false;
         let mut value = 0;
-        self.board_state();
         while !valid_square {
-            println!("Turn: Player ( {letter} ) \n");
             eprint!("Enter your move(1-9): ");
             let mut pinput = String::new();
 
@@ -91,31 +98,62 @@ impl OX {
                 .parse()
                 .expect("Man please enter numbers only ");
             value = pinput;
-            if self.available_moves().contains(&pinput) {
+            if available_moves.contains(&pinput) {
                 valid_square = true;
             } else {
-                self.board_state();
-                println!("OX: Baka ! That's an Invalid Move \n");
-                println!("Available Moves: {:?}\n", self.available_moves());
+                OX::board_state(state.clone());
+                println!("\nOX: Baka ! That's an Invalid Move \n");
+
+                println!("Available Moves: {:?}\n", available_moves);
+
+                println!("Turn: Player ( {} )\n", letter);
             }
         }
 
         value
     }
 }
-impl Player {}
+
+impl DumbComputer {
+    fn get_move(available_moves: Vec<usize>) -> usize {
+        let mut valid_square = false;
+        let mut val = 0;
+        while !valid_square {
+            let mut rng = rand::rng();
+            let square = rng.random_range(1..=9);
+            val = square;
+
+            if available_moves.contains(&square) {
+                valid_square = true;
+            }
+        }
+        val
+    }
+}
 fn main_game() {
     let mut board = OX(['_'; 9]);
-    board.board_state();
+
+    OX::board_state(board.state());
     let mut letter = 'X';
+    DumbComputer::get_move(board.available_moves());
     while board.empty_squares() != 0 {
-        board.manuplicate_ox(board.get_move(letter), letter);
+        println!("Turn: Player ( {} )\n", letter);
+        if letter == 'X' {
+            board.manuplicate_ox(
+                Player::get_move(board.available_moves(), board.state(), letter),
+                letter,
+            );
+            OX::board_state(board.state());
+        } else {
+            board.manuplicate_ox(DumbComputer::get_move(board.available_moves()), letter);
+            OX::board_state(board.state());
+        }
+
         if board.win_check(letter) {
-            board.board_state();
             println!("Yay, Player  ( {letter} ) won !");
             break;
         } else if board.empty_squares() == 0 {
-            board.board_state();
+            OX::board_state(board.state());
             println!("OX: it's a draw ");
             break;
         }
